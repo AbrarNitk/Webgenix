c49a24187825d469e2a55b17b70f81d86eccc6e8
Abrar Khan <abrar.nitk@gmail.com>
2023-09-25 19:30:59 +0530

    Initial commit

---
+++ service/src/utils.rs
@@ -0,0 +1,19 @@
+pub fn read_env() -> String {
+    match std::env::var("ENV") {
+        Ok(env) => env.to_lowercase(),
+        Err(_) => "local".to_string(),
+    }
+}
+
+pub fn read_port_env() -> u16 {
+    let port = match std::env::var("PORT") {
+        Ok(env) => env.to_lowercase(),
+        Err(_) => "8000".to_string(),
+    };
+
+    port.parse().expect(format!("cannot parse port: {port}").as_str())
+}
+
+pub fn is_traced() -> bool {
+    std::env::var("TRACING").is_ok() || std::env::args().any(|e| e == "--trace")
+}
